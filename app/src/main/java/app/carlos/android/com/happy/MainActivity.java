fb276d6da43e2671f50eaff50546344724da714c
Carlos Mendes <carlosepmendes@gmail.com>
2014-07-12 00:55:40 +0000

    changed layout and added languages

--- app/src/main/java/app/carlos/android/com/happy/MainActivity.java
+++ app/src/main/java/app/carlos/android/com/happy/MainActivity.java
@@ -4,6 +4,7 @@ import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
 
 
 public class MainActivity extends ActionBarActivity {
@@ -32,5 +33,8 @@ public class MainActivity extends ActionBarActivity {
             return true;
         }
         return super.onOptionsItemSelected(item);
+    }
+
+    public void onYesButtonClick(View view) {
     }
 }
